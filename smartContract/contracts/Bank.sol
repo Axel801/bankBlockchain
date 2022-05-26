9a3dd5e00b8b5e063199d4f67c8e2ae8dc361d68
Axel801 <axelloupias@gmail.com>
2022-05-26 20:31:30 +0200

    Downgrade solidity compiler (web3j in windows problems)

--- smartContract/contracts/Bank.sol
+++ smartContract/contracts/Bank.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-import "./SafeMath.sol";
+pragma solidity ^0.7.1;
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Bank {
     using SafeMath for uint256;
