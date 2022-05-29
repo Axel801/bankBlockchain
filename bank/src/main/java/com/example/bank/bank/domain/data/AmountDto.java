17946efe5fb61fae972c316d3037dcaa487c5905
Axel801 <axelloupias@gmail.com>
2022-05-29 12:09:21 +0200

    Fix last commit

---
+++ bank/src/main/java/com/example/bank/bank/domain/data/AmountDto.java
@@ -0,0 +1,17 @@
+package com.example.bank.bank.domain.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AmountDto {
+
+    private Long id;
+
+    private double amount;
+}
